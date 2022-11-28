f05711425558663ba332c126df03f257536e6d3a
Mumtaza15 <ahmad.fatha1502@mail.ugm.ac.id>
2022-11-28 19:30:14 +0700

    semoga berhasilr

--- resources/views/buku/create.blade.php
+++ resources/views/buku/create.blade.php
@@ -29,20 +29,3 @@
 <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-
-<!-- <script>
-  $( function() {
-    $( "#datepicker" ).datepicker();
-  } );
-  </script> -->
-
-
-<!-- <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-<script type="text/javascript">
-$('#date').datepicker({
-    format: 'yyyy/mm/dd',
-    autoclose: 'true'
-});
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    -->
